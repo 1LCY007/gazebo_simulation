894237ef85b3725098e1526ffd3a2c86bed9baa5
1LCY007 <344365733@qq.com>
2026-01-09 18:27:56 +0800

    1

---
+++ unitree_legged_sdk/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/luo007/dog_ws/src/unitree_legged_sdk/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/luo007/dog_ws/src/unitree_legged_sdk/build/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/luo007/dog_ws/src/unitree_legged_sdk/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/luo007/dog_ws/src/unitree_legged_sdk/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH='/home/luo007/dog_ws/src/unitree_legged_sdk:/home/luo007/ws_rocopar/src:/home/luo007/lcy/test_ws/src:/home/luo007/catkin_ws/src:/opt/ros/noetic/share'
